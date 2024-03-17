baf5651d085f6e528b6490062eaec51f78ca97e0
galor <galor.dvlp@gmail.com>
2024-03-17 17:10:37 +0200

    Add more symbols component in appHeader, update width property in filter

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 
 import { EmailFilter } from "../cmps/EmailFilter"
+import { MoreActions } from './MoreActions';
 
 import { GiHamburgerMenu } from "react-icons/gi";
 
@@ -14,12 +15,10 @@ export function AppHeader({ filterBy, onSetFilter }) {
                 <GiHamburgerMenu className="menu-icon" />
                 <img className="gmail-logo" src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png" srcSet="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r5.png 1x, https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_2x_r5.png 2x " alt="" aria-hidden="true" role="presentation" ></img>
             </div>
-            <EmailFilter filterBy={filterBy} onSetFilter={onSetFilter} />
-            {/* <section className="links-container">
-                <Link to="/" className='link'>Home</Link>
-                <Link to="/about" className='link'>About</Link>
-                <Link to="/email" className='link'>Emails</Link>
-            </section> */}
+            <div className="center-div">
+                <EmailFilter filterBy={filterBy} onSetFilter={onSetFilter} />
+                <MoreActions />
+            </div>
         </header>
     )
 }
---
+++ src/cmps/MoreActions.jsx
@@ -0,0 +1,16 @@
+import { Icon } from './Icon.jsx'
+import path from '../services/image-path.js'
+
+export function MoreActions() {
+
+    return (
+        <section className="more-symbols">
+
+            <Icon path={path.googleApps} />
+            <Icon path={path.googleApps} />
+            <Icon path={path.googleApps} />
+
+
+        </section>
+    )
+}
